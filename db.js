3b68aac2aea73cdaf7f8a831029ba9f704c88148
DKDEV <dkgamelan@gmail.com>
2024-02-19 17:51:11 -0300

    v10

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
-  process.env.DB_PASSWORD, {
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     port: 5432
   }
