1deef437d558dc2280cdd8acbbb5808657dcf5ea
DKDEV <dkgamelan@gmail.com>
2024-02-19 16:35:29 -0300

    V2

--- db.js
+++ db.js
@@ -1,14 +1,8 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD, {
-    dialect: 'postgres',
-    port: parseInt(process.env.DB_PORT)
-  }
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres'
+});
