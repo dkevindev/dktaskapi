093407c676cce7a816f3e6a33a75c8d6e70d5c34
DKDEV <dkgamelan@gmail.com>
2024-02-28 23:18:27 -0300

    v13

---
+++ db copy.js
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+
+
+export const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: 5432
+  }
+);
+
+
--- models/model.js
+++ models/model.js
@@ -1,5 +1,19 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../db.js'
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: 5432
+  }
+);
+
+
+
 
 // Tabela de Usuários
 export const Usuario = sequelize.define('Usuario', {
